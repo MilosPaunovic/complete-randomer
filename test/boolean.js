daba48ea6406505a5eebde5a694f5564594ccf3a
Milos Paunovic <paun992@hotmail.com>
2021-04-25 17:50:35 +0200

    Minor adjustment to order of tests

--- test/boolean.js
+++ test/boolean.js
@@ -2,6 +2,10 @@ const { expect, assert } = require('chai');
 const BOOLEAN = require('../modules/boolean');
 
 describe('BOOLEAN()', function () {
+  it('Should throw TypeError for containing parameters', function () {
+    assert.throw(() => { BOOLEAN(true) }, TypeError, 'No parameters allowed');
+  });
+
   it('Should return random value', function () {
     const result = BOOLEAN();
     expect(result).to.be.oneOf([true, false]);
@@ -10,9 +14,5 @@ describe('BOOLEAN()', function () {
   it('Should test if returned value is Boolean', function () {
     const result = BOOLEAN();
     assert.typeOf(result, 'boolean');
-  });
-
-  it('Should throw TypeError for containing parameters', function () {
-    assert.throw(() => { BOOLEAN(true) }, TypeError, 'No parameters allowed');
   });
 });
