c4f4aff10a64d57e93a650099424c0d7e88168d9
Milos Paunovic <paun992@hotmail.com>
2021-04-25 13:01:55 +0200

    Added entry point with INTEGER function

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Returns random generated integer between sent values, inclusively.
+ *
+ * @param {Number} min Minimum value for random number
+ * @param {Number} max Maximum value for random number
+ * @return {Number} Resulting number
+ */
+function INTEGER(min, max) {
+  const isArgumentInvalid = (arg) => typeof arg !== 'number';
+
+  if ([...arguments].filter(isArgumentInvalid).length > 0) {
+    throw new TypeError('Both parameters must be of Number type.');
+  }
+
+  return Number(Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+module.exports = { INTEGER };
