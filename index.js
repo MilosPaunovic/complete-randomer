f953765dea755639a579ec1ec9a2f10c409fb4cf
Milos Paunovic <paun992@hotmail.com>
2021-04-25 17:47:22 +0200

    Moved all functions to separate modules

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
 const BOOLEAN = require('./modules/boolean')
-
-/**
- * Returns random generated integer between sent values, inclusively
- *
- * @param {Number} min Minimum value for random number
- * @param {Number} max Maximum value for random number
- * @return {Number} Resulting number
- */
-function INTEGER(min, max) {
-  const isArgumentInvalid = (arg) => typeof arg !== 'number';
-
-  if ([...arguments].filter(isArgumentInvalid).length > 0) {
-    throw new TypeError('Both parameters must be Number');
-  }
-
-  return Number(Math.floor(Math.random() * (max - min + 1)) + min);
-}
-
-/**
- * Returns random generated string containing number of characters
- * forwarded in function parameter
- *
- * @param {Number} length Number of character for your random string
- * @return {String} Resulting string
- */
-function STRING(length) {
-  const isArgumentInvalid = (arg) => typeof arg !== 'number';
-
-  if ([...arguments].filter(isArgumentInvalid).length > 0) {
-    throw new TypeError('Length parameter must be Number');
-  }
-
-  let string = '';
-
-  while (string.length < length) {
-    string += Math.random()
-      .toString(36)
-      .substr(2, length - string.length);
-  }
-
-  return string;
-}
+const INTEGER = require('./modules/number')
+const STRING = require('./modules/string')
 
 module.exports = { INTEGER, STRING, BOOLEAN };
---
+++ modules/string.js
@@ -0,0 +1,24 @@
+/**
+ * Returns random generated string containing number of characters
+ * forwarded in function parameter
+ *
+ * @param {Number} length Number of character for your random string
+ * @return {String} Resulting string
+ */
+function STRING(length) {
+    const isArgumentInvalid = (arg) => typeof arg !== 'number';
+
+    if ([...arguments].filter(isArgumentInvalid).length > 0) {
+        throw new TypeError('Length parameter must be Number');
+    }
+
+    let string = '';
+
+    while (string.length < length) {
+        string += Math.random()
+            .toString(36)
+            .substr(2, length - string.length);
+    }
+
+    return string;
+}
