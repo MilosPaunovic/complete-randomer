a19640617dd6c713861d8b234390041d5832f2a8
Milos Paunovic <paun992@hotmail.com>
2021-04-27 22:46:17 +0200

    Dividing tests for COLOR functions into separate describe blocks

--- test/color.js
+++ test/color.js
@@ -9,69 +9,77 @@ const RGBA_REGEX = /^rgb\((\d{1,3}) (\d{1,3}) (\d{1,3}) \/ ([\d.]+)%\)$/;
 const HSL_REGEX = /^hsl\((\d{1,3}) ([\d.]+)% ([\d.]+)%\)$/;
 const HSLA_REGEX = /^hsl\((\d{1,3}) ([\d.]+)% ([\d.]+)% \/ ([\d.]+)%\)$/;
 
-describe('COLOR()', () => {
-  it('HEX() - Should throw TypeError if parameters are sent', () => {
+describe('HEX()', () => {
+  it('Should throw TypeError if parameters are sent', () => {
     assert.throw(() => { HEX(true); }, TypeError, '0 parameter(s) allowed');
   });
 
-  it('HEX() - Should return random HEX color', () => {
+  it('Should return random HEX color', () => {
     const result = HEX_REGEX.test(HEX());
     expect(result).to.equal(true);
   });
 
-  it('HEX() - Should test if returned value is String', () => {
+  it('Should test if returned value is String', () => {
     assert.isString(HEX());
   });
+});
 
-  it('RGB() - Should throw TypeError if parameters are sent', () => {
+describe('RGB()', () => {
+  it('Should throw TypeError if parameters are sent', () => {
     assert.throw(() => { RGB(true); }, TypeError, '0 parameter(s) allowed');
   });
 
-  it('RGB() - Should return random RGB color', () => {
+  it('Should return random RGB color', () => {
     const result = RGB_REGEX.test(RGB());
     expect(result).to.equal(true);
   });
 
-  it('RGB() - Should test if returned value is String', () => {
+  it('Should test if returned value is String', () => {
     assert.isString(RGB());
   });
+});
 
-  it('RGBA() - Should throw TypeError if parameters are sent', () => {
+describe('RGBA()', () => {
+  it('Should throw TypeError if parameters are sent', () => {
     assert.throw(() => { RGBA(true); }, TypeError, '0 parameter(s) allowed');
   });
 
-  it('RGBA() - Should return random RGBA color', () => {
+  it('Should return random RGBA color', () => {
     const result = RGBA_REGEX.test(RGBA());
     expect(result).to.equal(true);
   });
 
-  it('RGBA() - Should test if returned value is String', () => {
+  it('Should test if returned value is String', () => {
     assert.isString(RGBA());
   });
+});
 
-  it('HSL() - Should throw TypeError if parameters are sent', () => {
+describe('HSL()', () => {
+  it('Should throw TypeError if parameters are sent', () => {
     assert.throw(() => { HSL(true); }, TypeError, '0 parameter(s) allowed');
   });
 
-  it('HSL() - Should return random HSL color', () => {
+  it('Should return random HSL color', () => {
     const result = HSL_REGEX.test(HSL());
     expect(result).to.equal(true);
   });
 
-  it('HSL() - Should test if returned value is String', () => {
+  it('Should test if returned value is String', () => {
     assert.isString(HSL());
   });
+});
 
-  it('HSLA() - Should throw TypeError if parameters are sent', () => {
+describe('HSLA()', () => {
+  it('Should throw TypeError if parameters are sent', () => {
     assert.throw(() => { HSLA(true); }, TypeError, '0 parameter(s) allowed');
   });
 
-  it('HSLA() - Should return random HSLA color', () => {
+  it('Should return random HSLA color', () => {
     const result = HSLA_REGEX.test(HSLA());
     expect(result).to.equal(true);
   });
 
-  it('HSLA() - Should test if returned value is String', () => {
+  it('Should test if returned value is String', () => {
     assert.isString(HSLA());
   });
 });
