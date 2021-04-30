3ad090e78ca438cefac653cfbe4814306ce124a6
Milos Paunovic <paun992@hotmail.com>
2021-05-01 00:21:06 +0200

    Added test for list of emails function

---
+++ test/email/list.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+const { LIST } = require('../../modules/email');
+
+describe('LIST()', () => {
+  it('Should throw TypeError if more than 1 parameter is sent', () => {
+    assert.throw(() => { LIST(true, true); }, TypeError, '1 parameter(s) allowed');
+  });
+
+  it('Should throw TypeError if parameter is not Number', () => {
+    assert.throw(() => { LIST(true); }, TypeError, 'Parameter(s) must be of -number- type');
+  });
+
+  it('Should throw Error if parameter is smaller than 1', () => {
+    assert.throw(() => { LIST(0); }, Error, 'Minimum length is 1');
+  });
+
+  it('Should throw Error if parameter is larger than 1000', () => {
+    assert.throw(() => { LIST(1001); }, Error, 'Maximum length is 1000');
+  });
+
+  it('Should return 10 emails if there are no parameter sent', () => {
+    const result = LIST();
+    assert.lengthOf(result, 10);
+  });
+
+  it('Should return 30 emails when parameter is sent', () => {
+    const result = LIST(30);
+    assert.lengthOf(result, 30);
+  });
+
+  it('Should test if returned value is Array', () => {
+    const result = LIST();
+    assert.isArray(result);
+  });
+});
