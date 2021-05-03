6e467de3fbdd41457dff8de85a662a774562b0b2
Milos Paunovic <paun992@hotmail.com>
2021-05-03 16:26:02 +0200

    Completed testing for user LIST function

---
+++ test/user/list.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+const { LIST } = require('../../modules/user');
+
+describe('LIST()', () => {
+  it('Should throw TypeError if more than 1 parameter is sent', () => {
+    assert.throw(() => { LIST(true, true); }, TypeError, '1 parameter(s) allowed');
+  });
+
+  it('Should throw TypeError if parameter is not Number', () => {
+    assert.throw(() => { LIST(true); }, TypeError, 'Parameter(s) must be of -number- type');
+  });
+
+  it('Should throw Error if parameter is smaller than 1', () => {
+    assert.throw(() => { LIST(0); }, Error, 'Number must be between 1 and 1000');
+  });
+
+  it('Should throw Error if parameter is larger than 1000', () => {
+    assert.throw(() => { LIST(1001); }, Error, 'Number must be between 1 and 1000');
+  });
+
+  it('Should return 10 user objects if there are no parameter sent', () => {
+    const result = LIST();
+    assert.lengthOf(result, 10);
+  });
+
+  it('Should return 30 user objects when parameter is sent', () => {
+    const result = LIST(30);
+    assert.lengthOf(result, 30);
+  });
+
+  it('Should test if returned value is Array', () => {
+    const result = LIST();
+    assert.isArray(result);
+  });
+});
