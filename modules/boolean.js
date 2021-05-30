1ef0f368c72bd6fab7b2f72ae45f4901bf0ab2d4
Milos Paunovic <paun992@hotmail.com>
2021-05-30 17:30:31 +0200

    Added YES_NO method to boolean module

--- modules/boolean.js
+++ modules/boolean.js
@@ -6,13 +6,42 @@ const { ARGUMENTS } = require('../utils/arguments');
  *
  * @return {Boolean} Resulting value
  */
+const generateBoolean = () => {
+  // Creating value
+  const value = Math.round(Math.random() * 1) === 0;
+
+  // Returning value
+  return value;
+};
+
+/**
+ * Returns random generated boolean value
+ *
+ * @return {Boolean} Resulting value
+ */
 exports.IS = function () {
   // Arguments checking
   ARGUMENTS(arguments, 0, 0, undefined, undefined);
 
   // Creating value
-  const value = Math.round(Math.random() * 1) === 0;
+  const value = generateBoolean();
 
   // Making sure value is casted to proper type
   return Boolean(value);
+};
+
+/**
+ * Returns random generated YES or NO string
+ *
+ * @return {String} Resulting YES or NO value
+ */
+exports.YES_NO = function () {
+  // Arguments checking
+  ARGUMENTS(arguments, 0, 0, undefined, undefined);
+
+  // Creating value
+  const value = generateBoolean() ? 'YES' : 'NO';
+
+  // Making sure value is casted to proper type
+  return String(value);
 };
